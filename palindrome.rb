d273a23c18d44c5f6ed8c3e7ab848a0f7edf81b1
Valerie Sui <valerie.k.sui@gmail.com>
2020-05-25 22:27:11 -0700

    add palindrome for show

---
+++ palindrome.rb
@@ -0,0 +1,10 @@
+## Given a string, writes a function that checks to see if a word is a palindrome
+# palindromee = a word or phrase that is the same forwards and backwards
+#
+# The palindrome does not have to be a dictionary wrod
+#
+# Ex: Input: dad / Output: true
+#
+#
+#Chekc to see if a permutation of it is a palindrome
+#Ex: Input: Tact Coa / Output: 'atco cta', 'taco cat'
