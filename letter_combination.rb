cc002fadd53ea6c26eb3529dd333a8630df11de9
Valerie Sui <valerie.k.sui@gmail.com>
2020-06-09 23:15:57 -0700

    iterative solution for phone letter mappings

--- letter_combination.rb
+++ letter_combination.rb
@@ -3,28 +3,44 @@
 #A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 #
  mapping = {
-  2: "abc",
-  3: 'def',
-  4: 'ghi',
-  5: 'jkl',
-  6: 'mno',
-  7: 'pqrs',
-  8: 'tuv',
-  9: 'wxyz'
- }.with_indifferent_access
+  "2"=> "abc",
+  "3"=> 'def',
+  "4"=> 'ghi',
+  "5"=> 'jkl',
+  "6"=> 'mno',
+  "7"=> 'pqrs',
+  "8"=> 'tuv',
+  "9"=> 'wxyz'
+ }
 #
 #Input: "23"
 #Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 #
-def mapping(numbers)
-  combinations = []
+def mappings(numbers, mapping)
+  length = numbers.length
+  return [] if length == 0
 
-  numbers.split.each do |num|
-    combinations += one_or_zero(num)
+  numbers_arr = numbers.split("")[1..-1]
+  combinations = mapping[numbers[0]].split("")
+  count = 0
+
+  numbers_arr.each_with_index do |num, idx|
+    mapped = mapping[num].split("")
+    tempMap = []
+    mapped.each do |m|
+      temp = combinations.map do |c|
+        puts c+m
+        count +=1
+        c + m
+      end
+      tempMap = tempMap.concat(temp)
+      puts "TM: #{tempMap}"
+    end
+    combinations = tempMap
+    puts "Comb: #{combinations}"
   end
+
+  puts "Count: #{count}"
+  combinations
 end
 
-def one_or_zero(number, rest)
-  return [] if numbers.length == 0
-  return mapping(numbers).split if numbers == 1
-end
